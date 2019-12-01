81f37f7c14c41474dcd117ab1cc8f931b598d637
coolPreye <kerebipreye@gmail.com>
2019-11-30 20:26:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Form, Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleChange = e => {
+    console.log(e);
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <div className="App" onSubmit={this.handleSubmit}>
+        <Container textAlign="center">
+          <Form size="small">
+            <Form.Field>
+              <input placeholder="name of video" width={6} onChange={this.handleChange} />
+            </Form.Field>
+            <Button type="submit">Search videos</Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
